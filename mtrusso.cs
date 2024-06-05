1413de04bdd7b6809a4c6aea5969d0445d927a8e
Gabrielly <yrbibz.who@gmail.com>
2024-06-05 17:24:19 -0300

    Exercicios em c#

---
+++ mtrusso.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n1, n2;
+
+Console.WriteLine("Digite um número: ");
+n1 =    int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um número maior caso queira ver belas palavras: ");
+n2 = int.Parse(Console.ReadLine());
+
+if  (n1 < n2)
+{
+
+    Console.WriteLine("Oy, navoruyu alkogol'");
+    Console.WriteLine("Slomal na kryshu zamok");
+    Console.WriteLine("Khochu navsegda s toboy");
+    Console.WriteLine("My tantsuyem pod lunoy");
+    Console.WriteLine("Ty kruche vsekh etikh zvozd");
+    Console.WriteLine("I dorozhe chem Rolls-Royce");
+    Console.WriteLine("Devochka s moikh snov teper' so mnoy");
+}
+else
+{
+    Console.WriteLine("Sem palavras bonitas para você");
+}
---
+++ primeiroexercicio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+
+//somando, subtraindo, dividindo e multiplicando em c algumacoisa
+
+int n1, n2;
+
+Console.Write("Digite o 1º número:  ");
+n1 = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o 2º número:  ");
+n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma 1º com o 2º é igual a: " + (n1+n2));
+Console.WriteLine("A subtração do 1º pelo 2º é igual a: " + (n1 - n2));
+Console.WriteLine("A multiplicação do 1º pelo 2º é igual a: " + (n1 * n2));
+Console.WriteLine("A divisão do 1º pelo 2º é igual a: " + (n1 / n2));
